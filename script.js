595f0c61687ca3ad1387b12ea642fe764b56c1ff
Dima <dmytrotarasov9@gmail.com>
2020-04-14 13:14:25 +0200

    задание 1

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+
+let money = prompt('Ваш бюджет на месяц?');
+
+let time = prompt('Введите дату в формате YYYY-MM-DD');
+let whatToBuy = prompt('Введите обязательную статью расходов в этом месяце');
+let howMuch = prompt('Во сколько обойдется?');
+
+
+let appData ={
+    moneyData: money,
+    timeData: time,
+    expenses: {
+        whatToBuy:howMuch
+    },
+    optionalExpenses:0,
+    income: [],
+    savings: false,
+    
+};
+
+let rest = (money - howMuch)/30;
+
+alert(`Бюджет на день: ${rest}`);
